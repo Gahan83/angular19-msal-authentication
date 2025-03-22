d79169425277fc4f1cce212127f1f29aa6aee0c0
Gahan Kotebagilu <Gahan.Kotebagilu@anthology.com>
2025-03-22 23:03:36 +0530

    Add Bootstrap for styling and implement authentication features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Microsoft-Msal-Authentication';
+export class AppComponent implements OnInit {
+  constructor(private msalService: MsalService) {}
+
+  ngOnInit(): void {
+    this.initializeMsal();
+  }
+
+  private async initializeMsal(): Promise<void> {
+    await this.msalService.instance.initialize();
+    try {
+      const result = await this.msalService.instance.handleRedirectPromise();
+      if (result?.account) {
+        this.msalService.instance.setActiveAccount(result.account);
+      }
+    } catch (error) {
+      console.error('Error during MSAL initialization:', error);
+    }
+  }
+
+  public isLoggedIn(): boolean {
+    return this.msalService.instance.getActiveAccount() !=  null;
+  }
+
+  public login():void {
+    //this.msalService.loginRedirect();
+    this.msalService.loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+        this.msalService.instance.setActiveAccount(response.account);
+      });
+  }
+
+  public logout(): void {
+    this.msalService.logout();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
 
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '69719854-0ddd-4ec9-b5ae-07f172e4a415',
+      authority: 'https://login.microsoftonline.com/4f250e01-3c2b-4951-a60e-a52af262c6b7',
+      redirectUri: 'http://localhost:4200'
+    }
+  });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideProtractorTestingSupport(),
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ]
 };
